4c8f10f9cc3526235daed56b93546fab5aadb4ae
aaaravv <aaravmishra619@gmail.com>
2024-08-18 12:29:14 +0530

    chore: Update homepage to use STORE_ID constant

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,27 +1,28 @@
-import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-products";
-import ProductList from "@/components/product-list";
-import Billboard from "@/components/ui/billboard";
-import Container from "@/components/ui/container";
+import getBillboard from '@/actions/get-billboard'
+import getProducts from '@/actions/get-products'
+import ProductList from '@/components/product-list'
+import Billboard from '@/components/ui/billboard'
+import Container from '@/components/ui/container'
+import { STORE_ID } from '@/config/constants'
 
-export const revalidate = 0;
+export const revalidate = 0
 
 const HomePage = async () => {
-  const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("d368861f-e9b0-4902-b98b-5142ceffadc4");
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard(STORE_ID)
 
   return (
     <Container>
       <div className="pb-10 space-y-10">
-        {Array.isArray(billboard) && <Billboard 
-          data={billboard}
-        />}
+        {Array.isArray(billboard) && <Billboard data={billboard} />}
         <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
-          {Array.isArray(products) && <ProductList title="Featured Products" items={products} />}
+          {Array.isArray(products) && (
+            <ProductList title="Featured Products" items={products} />
+          )}
         </div>
       </div>
     </Container>
   )
-};
+}
 
-export default HomePage;
+export default HomePage
--- ecommerce-store/config/constants.ts
+++ ecommerce-store/config/constants.ts
@@ -1,1 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'
+export const STORE_ID = API_URL?.split('/').pop() as string
